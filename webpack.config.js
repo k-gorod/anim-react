49bc11ec9cb23584e9b72dfe37f2be99bf9919b9
Kostya <k.gorodnichuk@gmail.com>
2022-09-05 00:40:33 +0300

    feat: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+    mode:"production",
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+        library: {
+            name: "anim-react",
+            type: "umd"
+        },
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+          { 
+            test: /\.(ts|tsx)$/,
+            loader: "ts-loader"
+          }
+        ]
+    },
+    externals: {
+        react: {
+          root: 'React',
+          commonjs2: 'react',
+          commonjs: 'react',
+          amd: 'react',
+          umd: 'react',
+        },
+        'react-dom': {
+          root: 'ReactDOM',
+          commonjs2: 'react-dom',
+          commonjs: 'react-dom',
+          amd: 'react-dom',
+          umd: 'react-dom',
+        },
+    },
+};
