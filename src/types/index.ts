341ca4c87e30416a4e3196c540360e4d5412dc93
Kostya <k.gorodnichuk@gmail.com>
2022-09-05 01:24:24 +0300

    add: animName types && autocomplite suggestions

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,8 +11,26 @@ export interface AnimConfigType extends KeyframeAnimationOptions {
 
 export type setAnimFooType = (props: boolean) => void
 
-export type animReturn = ({ config: AnimConfigType; setState: (activity: (isActive: boolean) => boolean | boolean) => void; updateConfig: (newConfigs: AnimConfigType) => void; hardStop:() => void; setEndCallback: (callback: () => any) => void})
+export type animReturn = ({
+  config: AnimConfigType; 
+  setState: (activity: (isActive: boolean) => boolean | boolean) => void;
+  updateConfig: (newConfigs: AnimConfigType) => void; hardStop:() => void;
+  setEndCallback: (callback: () => any) => void
+})
 
-export type animType = (props: {ref: refType, animName?: string, userConfig?: AnimConfigType }) => animReturn
+export type NamedAnimType = (
+  "slideFromLeft" |
+  "slideFormRight" |
+  "slideFromTop" |
+  "slideFromBottom" |
+  "opacityAppear" 
+)
+
+export type animType = (
+  props: {
+    ref: refType,
+    animName?: NamedAnimType,
+    userConfig?: AnimConfigType
+  }) => animReturn
 
 export type AnimInstT = React.MutableRefObject<Animation | null> ;
